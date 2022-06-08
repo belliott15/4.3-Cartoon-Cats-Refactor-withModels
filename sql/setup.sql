355d3424ef9bc783914a8c7d3e630268fb5ce8f1
Beau Elliott <belliott1516@gmail.com>
2022-06-08 11:26:47 -0700

    trying to get database to link up

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,12 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists cats;
+
+CREATE table cats (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,  
+    age INT NOT NULL
+);
+
+INSERT INTO cats (name, age) VALUES
+('demon', 2);
