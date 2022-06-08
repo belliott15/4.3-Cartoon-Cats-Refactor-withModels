5b8aad3349320ff7bc50d64757a832110e61da10
Beau Elliott <belliott1516@gmail.com>
2022-06-08 12:05:22 -0700

    added .env.example to gitignore

--- sql/setup.sql
+++ sql/setup.sql
@@ -4,11 +4,14 @@ DROP table if exists cats;
 
 CREATE table cats (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR NOT NULL,  
-    age INT NOT NULL
+    name VARCHAR NOT NULL,
+    type VARCHAR NOT NULL,
+    url VARCHAR NOT NULL,
+    year INT NOT NULL, 
+    lives INT NOT NULL,
+    isSidekick BOOLEAN NOT NULL
 );
 
-INSERT INTO cats (name, age) VALUES
-('demon', 2),
-('will', 4), 
-('cody', 3);
+INSERT INTO cats (name, type, url, year, lives, isSidekick) VALUES
+('Felix', 'Tuxedo', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Felix_the_cat.svg/200px-Felix_the_cat.svg.png', 1982, 3, FALSE),
+('Felix', 'Tuxedo', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Felix_the_cat.svg/200px-Felix_the_cat.svg.png', 1982, 3, FALSE);
